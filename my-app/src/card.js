8e55135eae71e2bac23d5bf3c163e87a17b19eb7
SabyGil <moetainment13@gmail.com>
2017-10-26 21:22:12 -0400

    First commit

---
+++ my-app/src/card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './card.css';
+
+
+export default function Card(){
+	const text = 'Blastoid';
+
+	return ( //jsx
+		<div className='card'>
+			{text}
+		</div>
+	)
+}
+
